97c031040e7b2aef365426f38fa26ea4663865ae
sjh709 <42454759+sjh709@users.noreply.github.com>
2022-10-13 01:43:45 +0900

    Create 38.java

---
+++ Level-0/38.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long solution(int balls, int share) {
+        if(share == 0) return 1;
+        long answer = solution(balls - 1, share - 1);
+        answer *= balls;
+        answer /= share;
+        return answer;
+    }
+}
