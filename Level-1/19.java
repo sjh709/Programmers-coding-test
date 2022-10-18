b852ec5b902a4ee0d17597454628a78c5f2b403b
Jihee <42454759+sjh709@users.noreply.github.com>
2022-10-18 19:37:53 +0900

    Create 19.java

---
+++ Level-1/19.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(long n) {
+        String s = String.valueOf(n);
+        int len = s.length();
+        int[] answer = new int[len];
+        for(int i = 0; i < len; i++){
+            answer[i] = s.charAt(len - (i + 1)) - '0';
+        }
+        return answer;
+    }
+}
