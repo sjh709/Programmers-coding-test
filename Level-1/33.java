2a26d5a8f3d89090be6ea25fcec6067f2fa3a397
Jihee <42454759+sjh709@users.noreply.github.com>
2022-10-20 15:50:50 +0900

    Create 33.java

---
+++ Level-1/33.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int price = 0;
+        Arrays.sort(d);
+        for(int i = 0; i < d.length; i++){
+            price += d[i];
+            if(price <= budget) answer++;
+        }
+        
+        return answer;
+    }
+}
