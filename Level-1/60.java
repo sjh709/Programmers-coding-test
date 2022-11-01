0245d6121e8d1863d1dfecb156a10452447f3fb2
Jihee <42454759+sjh709@users.noreply.github.com>
2022-11-01 20:28:06 +0900

    Create 60.java

---
+++ Level-1/60.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] ok = {"aya", "ye", "woo", "ma"};
+        String[] no = {"ayaaya", "yeye", "woowoo", "mama"};
+        
+        for(int i = 0; i < babbling.length; i++){
+            for(int j = 0; j < no.length; j++){
+                babbling[i] = babbling[i].replace(no[j], "x");
+            }
+            
+            for(int j = 0; j < ok.length; j++){
+                babbling[i] = babbling[i].replace(ok[j], " ");
+            }
+        }
+        
+        for(String s : babbling){
+            if(s.trim().length() == 0) answer += 1;
+        }
+        
+        return answer;
+    }
+}
