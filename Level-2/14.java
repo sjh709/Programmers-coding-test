8369adae727736bbfca733d70bd816d4f5acda6c
Jihee <42454759+sjh709@users.noreply.github.com>
2022-11-13 00:27:55 +0900

    Update 14.java

--- Level-2/14.java
+++ Level-2/14.java
@@ -1,1 +1,24 @@
-
+import java.util.*;
+class Solution {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        long fac = 1;
+        for(int i = 1; i <= n; i++){
+            list.add(i);
+            fac *= i;
+        }
+        int idx = 0;
+        k--;
+        while(true){
+            fac /= (n - idx);
+            long value = k / fac;
+            answer[idx++] = list.get((int)value);
+            list.remove((int)value);
+            if(idx == n - 1) break;
+            k %= fac;
+        }
+        answer[idx] = list.get(0);
+        return answer;
+    }
+}
