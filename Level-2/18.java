fbc55c716fb95a02ca6485d63350c1da81d13539
Jihee <42454759+sjh709@users.noreply.github.com>
2022-11-15 01:39:01 +0900

    Update 18.java

--- Level-2/18.java
+++ Level-2/18.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int[][] answer;
+    public int x = 0;
+    public void hanoi(int num, int from, int other, int to) {
+        if(num == 0) return;
+        hanoi(num - 1, from, to, other);
+        answer[x][0] = from;
+        answer[x++][1] = to;
+        hanoi(num - 1, other, from, to);
+    }
+    public int[][] solution(int n) {
+        int size = (int) Math.pow(2, n) - 1;
+        answer = new int[size][2];
+        hanoi(n, 1, 2, 3);
+        return answer;
+    }
+}
