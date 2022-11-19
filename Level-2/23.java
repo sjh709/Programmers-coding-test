772d011490241a4d656b14d48752d899d47895ce
Jihee <42454759+sjh709@users.noreply.github.com>
2022-11-20 01:56:12 +0900

    Create 23.java

---
+++ Level-2/23.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(stack.isEmpty()){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.peek() == s.charAt(i)){
+                    stack.pop();
+                }else{
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        answer = stack.size() == 0 ? 1 : 0;
+        return answer;
+    }
+}
