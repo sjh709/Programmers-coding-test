61e318fc05f3e7e81cc07705d59b13cc29aa2c62
Jihee <42454759+sjh709@users.noreply.github.com>
2022-11-23 23:41:19 +0900

    Create 27.java

---
+++ Level-2/27.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int n, int a, int b) {
+        int answer = 1;
+        
+        int left = 0;
+        int right = 0;
+
+        if(a < b){
+            left = a;
+            right = b;
+        }else{
+            left = b;
+            right = a;
+        }
+        
+        while(true){
+            if(left % 2 != 0 && right - left == 1){
+                break;
+            }
+            left = (left + 1) / 2;
+            right = (right + 1) / 2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
