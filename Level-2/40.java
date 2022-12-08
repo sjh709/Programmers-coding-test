f129ce04bcce8bc86b79254937a19b4a1724f175
Jihee <42454759+sjh709@users.noreply.github.com>
2022-12-09 03:26:43 +0900

    Update 40.java

--- Level-2/40.java
+++ Level-2/40.java
@@ -1,1 +1,58 @@
+import java.util.*;
 
+class Node {
+    private int x;
+    private int y;
+    
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {
+        return this.x;
+    }
+    
+    public int getY() {
+        return this.y;
+    }
+}
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        
+        Queue<Node> q = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < priorities.length; i++) {
+            q.offer(new Node(i, priorities[i]));
+        }
+        
+        while(!q.isEmpty()) {
+            boolean check = false;
+            Node node = q.poll();
+            int x = node.getX();
+            int y = node.getY();
+            for(Node n : q) {
+                if(y < n.getY()) {
+                    check = true;
+                    break; 
+                }
+            }
+            if(check) {
+                q.offer(new Node(x, y));
+            }else {
+                list.add(x);
+            }
+        }
+        
+        for(int i = 0; i < list.size(); i++) {
+            if(location == list.get(i)) {
+                answer = i + 1;
+            }
+        }
+        
+        return answer;
+    }
+}
