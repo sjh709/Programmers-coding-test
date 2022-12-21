70d98120e97d64fe254cf7f526e1331e6fbd682b
Jihee <42454759+sjh709@users.noreply.github.com>
2022-12-21 22:26:09 +0900

    Update 56.java

--- Level-2/56.java
+++ Level-2/56.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public long solution(int w, int h) {
+        long answer = w;
+        
+        int wt = w;
+        int ht = h;
+        
+        while(ht != 0) {
+            int r = wt % ht;
+            wt = ht;
+            ht = r;
+        }
+        
+        //answer = (long)w * h - ((w / wt) + (h / wt) - 1) * wt;
+        answer *= h;
+        answer -= w + h - wt;
+        
+        return answer;
+    }
+}
