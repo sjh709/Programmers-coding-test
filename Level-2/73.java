ca1f0df6b81534583e51d5e2aa8a7689d8ee326a
Jihee <42454759+sjh709@users.noreply.github.com>
2023-01-04 17:46:40 +0900

    Update 73.java

--- Level-2/73.java
+++ Level-2/73.java
@@ -1,1 +1,25 @@
-
+class Solution {
+    
+    public boolean[] visit;
+    public int answer;
+    
+    public void backTracking(int depth, int k, int[][] dungeons) {
+        for(int i = 0; i < dungeons.length; i++) {
+            if(!visit[i] && dungeons[i][0] <= k) {
+                visit[i] = true;
+                backTracking(depth + 1, k - dungeons[i][1], dungeons);
+                visit[i] = false;
+            }
+        }
+        answer = Math.max(answer, depth);
+    }
+    
+    public int solution(int k, int[][] dungeons) {
+        answer = 0;
+        visit = new boolean[dungeons.length];
+        
+        backTracking(0, k, dungeons);
+        
+        return answer;
+    }
+}
