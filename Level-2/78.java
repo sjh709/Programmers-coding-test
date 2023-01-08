2103412e39de13ad57f7c4b7b8823d58d699e3dd
Jihee <42454759+sjh709@users.noreply.github.com>
2023-01-08 19:30:12 +0900

    Update 78.java

--- Level-2/78.java
+++ Level-2/78.java
@@ -1,1 +1,33 @@
+import java.util.*;
 
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        
+        Map<String, Integer> map;
+        for(int i = 0; i <= discount.length - 10; i++) {
+            boolean check = true;
+            map = new HashMap<>();
+            for(int k = 0; k < want.length; k++) {
+                map.put(want[k], number[k]);
+            }
+            
+            for(int j = i; j < i + 10; j++) {
+                if(map.containsKey(discount[j]) && map.get(discount[j]) > 0) {
+                    map.put(discount[j], map.get(discount[j]) - 1);
+                }
+            }
+            
+            for(int n : map.values()) {
+                if(n != 0) {
+                    check = false;
+                    break;
+                }
+            }
+            
+            if(check) answer++;
+        }
+        
+        return answer;
+    }
+}
