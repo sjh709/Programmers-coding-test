d8b7b02235f1b30e14cde75fc64a1b46a0ef19e5
Jihee <42454759+sjh709@users.noreply.github.com>
2023-02-21 01:28:55 +0900

    Update 100.java

--- Level-0/100.java
+++ Level-0/100.java
@@ -2,18 +2,15 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         String[] ok = {"aya", "ye", "woo", "ma"};
-        String[] no = {"ayaaya", "yeye", "woowoo", "mama"};
-        for(int i = 0; i < babbling.length; i++){
-            for(int j = 0; j < no.length; j++){
-                babbling[i] = babbling[i].replace(no[j], "x");
-            }
-            
-            for(int j = 0; j < ok.length; j++){
-                babbling[i] = babbling[i].replace(ok[j], "");
+        
+        for(int i = 0; i < babbling.length; i++) {
+            for(int j = 0; j < ok.length; j++) {
+                babbling[i] = babbling[i].replaceAll(ok[j], "A");
             }
         }
         
-        for(int i = 0; i < babbling.length; i++){
+        for(int i = 0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replaceAll("A", "");
             if(babbling[i].length() == 0) answer++;
         }
         
