b6752ece278682c0e6b398d4c717d00aa31c6ddb
Jihee <42454759+sjh709@users.noreply.github.com>
2023-09-23 00:01:12 +0900

    Update 38.js

--- Level-0/38.js
+++ Level-0/38.js
@@ -1,1 +1,6 @@
+const fact = (num) => (num === 0 ? 1 : fact(num - 1) * num);
 
+function solution(balls, share) {
+    const answer = fact(balls) / (fact(balls - share) * fact(share));
+    return Math.round(answer);
+}
