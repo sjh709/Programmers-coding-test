9d73b1ceae95dbe2fcb184c6198bc142293e0af7
Jihee <42454759+sjh709@users.noreply.github.com>
2023-10-09 20:29:46 +0900

    Create 53.js

---
+++ Level-0/53.js
@@ -0,0 +1,8 @@
+function solution(dots) {
+    let x = [], y = [];
+    for(let dot of dots) {
+        x.push(dot[0]);
+        y.push(dot[1]);
+    }
+    return (Math.max(...x) - Math.min(...x)) * (Math.max(...y) - Math.min(...y));
+}
