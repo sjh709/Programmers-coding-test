9888d0c17efcfdd3233cae792e9f79996fac6dbe
Jihee <42454759+sjh709@users.noreply.github.com>
2023-11-02 22:57:10 +0900

    Update 77.js

--- Level-0/77.js
+++ Level-0/77.js
@@ -1,1 +1,7 @@
-
+function solution(my_string) {
+    let answer = "";
+    for(let i of my_string) {
+        answer += i === i.toLowerCase() ? i.toUpperCase() : i.toLowerCase();
+    }
+    return answer;
+}
