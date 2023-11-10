267d167876ee59f9b8de4c2864a2cd816fa64360
Jihee <42454759+sjh709@users.noreply.github.com>
2023-11-11 02:08:34 +0900

    Update 84.js

--- Level-0/84.js
+++ Level-0/84.js
@@ -1,1 +1,20 @@
-
+function solution(my_string) {
+    let answer = 0;
+    let oper = '';
+    my_string.split(' ').forEach((v, i) => {
+        if(i === 0) {
+            answer += parseInt(v);
+        }else {
+            if(v === '+' || v === '-') {
+                oper = v;
+            }else {
+                if(oper === '+') {
+                    answer += parseInt(v);
+                }else if(oper === '-') {
+                    answer -= parseInt(v);
+                }
+            }
+        }
+    });
+    return answer;
+}
