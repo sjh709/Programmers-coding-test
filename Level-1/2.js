a79fb1207e6b33b38eb21abd0ea1dd05365c5139
Jihee <42454759+sjh709@users.noreply.github.com>
2023-12-05 01:57:50 +0900

    Update 2.js

--- Level-1/2.js
+++ Level-1/2.js
@@ -1,1 +1,13 @@
-
+function solution(a, b) {
+    const dayList = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'];
+    const monthArr = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let day = b - 1;
+    
+    if(a > 1) {
+        for(let i = 0; i < a - 1; i++) {
+            day += monthArr[i];
+        }
+    }
+    
+    return dayList[day % 7];
+}
