27315805316ff9b0e99d63599cd4e6a153a7c0fd
Jihee <42454759+sjh709@users.noreply.github.com>
2023-12-30 17:18:16 +0900

    Create 25.js

---
+++ Level-1/25.js
@@ -0,0 +1,8 @@
+function solution(num) {
+    let answer = 0;
+    while(num !== 1) {
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+        if(++answer > 500) return -1;
+    }
+    return answer;
+}
