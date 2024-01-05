6d2ae11a78f329de24997360c4be2af0b4fba6f0
Jihee <42454759+sjh709@users.noreply.github.com>
2024-01-06 02:48:39 +0900

    Update 32.js

--- Level-1/32.js
+++ Level-1/32.js
@@ -1,24 +1,21 @@
+function check(n) {
+    for(let i = 2; i <= Math.sqrt(n); i++) {
+        if(n % i === 0) return false;
+    }
+    
+    return true;
+}
+
 function solution(nums) {
-    let answer = [];
+    let cnt = 0
     
     for(let i = 0; i < nums.length - 2; i++) {
         for(let j = i + 1; j < nums.length - 1; j++) {
             for(let k = j + 1; k < nums.length; k++) {
-                answer.push(nums[i] + nums[j] + nums[k]);
+                check(nums[i] + nums[j] + nums[k]) ? cnt++ : '';
             }
         }
     }
     
-    let cnt = 0;
-    for(let i = 0; i < answer.length; i++) {
-        const n = answer[i];
-        for(let j = 2; j <= Math.sqrt(n); j++) {
-            if(n % j === 0) {
-                cnt++;
-                break;
-            }
-        }
-    }
-    
-    return answer.length - cnt;
+    return cnt;
 }
