6f48b5c9f8fe2daaf7d26d055fe0d128e5e883a3
Jihee <42454759+sjh709@users.noreply.github.com>
2024-01-15 10:17:05 +0900

    Create 51.js

---
+++ Level-2/51.js
@@ -0,0 +1,19 @@
+function solution(numbers, target) {
+    let answer = 0;
+    
+    const dfs = (count, sum) => {
+        if(count === numbers.length) {
+            if(sum === target) {
+                answer++;
+            }
+            return;
+        }
+        
+        dfs(count + 1, sum + numbers[count]);
+        dfs(count + 1, sum - numbers[count]);
+    };
+    
+    dfs(0, 0);
+    
+    return answer;
+}
