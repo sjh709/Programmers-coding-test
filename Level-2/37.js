a1fc995c516f591330cf99ad3f6c2c7f94eedb6d
Jihee <42454759+sjh709@users.noreply.github.com>
2024-01-26 13:24:29 +0900

    Create 37.js

---
+++ Level-2/37.js
@@ -0,0 +1,22 @@
+function solution(bridge_length, weight, truck_weights) {
+    let bridge = new Array(bridge_length).fill(0);
+    let time = 0;
+    
+    while(bridge.length) {
+        bridge.shift();
+        
+        if(truck_weights.length) {
+            const sumBridge = bridge.reduce((acc, cur) => acc + cur, 0);
+            
+            if(sumBridge + truck_weights[0] <= weight) {
+                bridge.push(truck_weights.shift());
+            }else {
+                bridge.push(0);
+            }
+        }
+        
+        time++;
+    }
+    
+    return time;
+}
