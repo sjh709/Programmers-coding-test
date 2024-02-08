5a107eb291e64e4ae4f289d5ace3bd6848e45887
Jihee <42454759+sjh709@users.noreply.github.com>
2024-02-08 16:59:26 +0900

    Update 64.js

--- Level-1/64.js
+++ Level-1/64.js
@@ -1,1 +1,15 @@
-
+function solution(number, limit, power) {
+    const attack = Array.from(new Array(number + 1), () => 0);
+    
+    for(let i = 1; i <= number; i++) {
+        for(let j = 1; j <= Math.sqrt(i); j++) {
+            if(i % j === 0) {
+                (j === i / j) ? attack[i] += 1 : attack[i] += 2;
+            }
+        }
+        
+        if(attack[i] > limit) attack[i] = power;
+    }
+    
+    return attack.reduce((acc, cur) => acc + cur, 0);
+}
