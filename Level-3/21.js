d26a6eb721333f127a3339b147c84138a4241b67
Jihee <42454759+sjh709@users.noreply.github.com>
2024-02-09 18:12:29 +0900

    Create 21.js

---
+++ Level-3/21.js
@@ -0,0 +1,15 @@
+function solution(triangle) {
+    for(let i = 1; i < triangle.length; i++) {
+        for(let j = 0; j < triangle[i].length; j++) {
+            if(j === 0) {
+                triangle[i][j] += triangle[i - 1][j];
+            }else if(j === i) {
+                triangle[i][j] += triangle[i - 1][j - 1];
+            }else {
+                triangle[i][j] += Math.max(triangle[i - 1][j], triangle[i - 1][j - 1]);
+            }
+        }
+    }
+    
+    return Math.max(...triangle[triangle.length - 1]);
+}
