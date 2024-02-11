c3eefeab4c289829c1051a84ade9abd5dc73f49f
Jihee <42454759+sjh709@users.noreply.github.com>
2024-02-11 15:22:01 +0900

    Update 66.js

--- Level-1/66.js
+++ Level-1/66.js
@@ -1,1 +1,25 @@
-
+function solution(s) {
+    let x = '';
+    let cnt1 = 0, cnt2 = 0;
+    let answer = 0;
+    
+    for(let c of s) {
+        if(x === '') x = c;
+        
+        if(x === c) {
+            cnt1++;
+        }else {
+            cnt2++;
+        }
+        
+        if(cnt1 === cnt2) {
+            answer++;
+            cnt1 = 0, cnt2 = 0;
+            x = '';
+        }
+    }
+    
+    if(x) answer++;
+    
+    return answer;
+}
