b0779736c79284ef5371ec568f792f3cac7f2407
Jihee <42454759+sjh709@users.noreply.github.com>
2024-02-13 22:19:30 +0900

    Update 67.js

--- Level-1/67.js
+++ Level-1/67.js
@@ -1,1 +1,16 @@
-
+function solution(s) {
+    let answer = [];
+    const map = new Map();
+    
+    for(let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if(map.has(c)) {
+            answer.push(i - map.get(c));
+        }else {
+            answer.push(-1);
+        }
+        map.set(c, i);
+    }
+    
+    return answer;
+}
