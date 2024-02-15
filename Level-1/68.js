d0e29c743653f26d4ce38665fe56b8b8d0ac552a
Jihee <42454759+sjh709@users.noreply.github.com>
2024-02-15 15:35:18 +0900

    Update 68.js

--- Level-1/68.js
+++ Level-1/68.js
@@ -1,1 +1,11 @@
-
+function solution(t, p) {
+    const length = p.length;
+    let answer = 0;
+    
+    for(let i = 0; i <= t.length - length; i++) {
+        const num = Number(t.substring(i, i + length));
+        if(num <= Number(p)) answer++;
+    }
+    
+    return answer;
+}
