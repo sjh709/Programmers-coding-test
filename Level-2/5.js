1fcc2aea8751e24a1cda1688b5e89fe87810ea93
Jihee <42454759+sjh709@users.noreply.github.com>
2024-03-03 18:25:50 +0900

    Create 5.js

---
+++ Level-2/5.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let dp = new Array(n + 1).fill(0);
+    dp[0] = 1; dp[1] = 1;
+    
+    for(let i = 2; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+    }
+    
+    return dp[n];
+}
