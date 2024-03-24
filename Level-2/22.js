e7dad276224fb42fb4880c2f1361ded09c52db75
Jihee <42454759+sjh709@users.noreply.github.com>
2024-03-25 02:48:00 +0900

    Create 22.js

---
+++ Level-2/22.js
@@ -0,0 +1,25 @@
+function solution(N, road, K) {
+    const arr = Array(N+1).fill(Number.MAX_SAFE_INTEGER);
+    const lines = Array.from(Array(N+1), () => []);
+    
+    road.forEach(([a, b, c]) => {
+        lines[a].push({to: b, cost: c});
+        lines[b].push({to: a, cost: c});
+    });
+    
+    let queue = [{to: 1, cost: 0}];
+    arr[1] = 0;
+    
+    while(queue.length) {
+        let {to} = queue.shift();
+        
+        lines[to].forEach((next) => {
+            if(arr[next.to] > arr[to] + next.cost) {
+                arr[next.to] = arr[to] + next.cost;
+                queue.push(next);
+            }
+        });
+    }
+    
+    return arr.filter((item) => item <= K).length;
+}
